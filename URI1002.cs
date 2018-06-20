91d7083c60e1245e02b0dbdf5443febfdc7b3e23
Victor Santana Costa <39810026+viictoorcoostah@users.noreply.github.com>
2018-06-20 01:44:02 -0300

    Add files via upload

---
+++ URI1002.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaBeginner2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159,r;
+
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("A= " + (n * r * r).ToString("0.0000"));
+
+            Console.ReadKey();
+            
+                        
+        }
+    }
+}
